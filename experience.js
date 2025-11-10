c53628d2e792216a7ffea3c9e50615e264e2a11a
Harshini <harshini.bannu@gmail.com>
2025-11-10 18:10:17 +0530

    Deleted all files

--- experience.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const cards = document.querySelectorAll(".experience-card");
-
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      card.classList.toggle("active");
-    });
-  });
-});
